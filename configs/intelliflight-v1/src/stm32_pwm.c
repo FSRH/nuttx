e0b04c8d63f2011712b44b866bcbfa9833ca0bc1
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2019-03-10 20:31:42 +0100

    add partial PWM support

---
+++ configs/intelliflight-v1/src/stm32_pwm.c
@@ -0,0 +1,23 @@
+/*
+ * intelliflight_v1_pwm.c
+ *
+ *  Created on: 09.03.2019
+ *      Author: hoelzlwimmerf
+ */
+
+
+
+
+static char *pwm_devices[] = {
+	DEV_PWMLED0,
+	DEV_PWM1,
+	DEV_PWM2,
+	DEV_PWM3,
+	DEV_PWM4,
+	DEV_PWM5,
+	DEV_PWM6,
+	DEV_PWM7,
+	DEV_PWM8,
+	DEV_PWM9
+};
+
